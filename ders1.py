918815c58292952374690d28f584bf39576b5018
Shahin Agayev <sh96agayev@gmail.com>
2023-04-06 19:15:58 +0400

    test

---
+++ ders1.py
@@ -0,0 +1,23 @@
+print('Hello world')
+
+name = 'Huseyn' 
+seher = 'Baki'
+
+print(name+' yaxsi oglandir ve o '+seher+' seherinde yasayir.')
+
+#if/else/elif
+
+a=5
+b=5
+if a==b:
+    print('deyisenler dogrudur')
+else:
+    print('deyisenler sefhdir')
+
+mekteb = 'bugun'
+if mekteb=='sabah':
+    print('sabah mektebe gedecem')
+elif mekteb=='bugun':
+    print('bugun mekteb var')
+else:
+    print('gorsen mektebe hacan gedecem')
